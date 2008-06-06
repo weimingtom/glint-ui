5763c3e91b0cae65a230caed0ae1472397dfe807
dtitov <dtitov@fc329b58-c54d-0410-8045-0749efdbdba4>
2008-06-06 01:25:38 +0000

    Make build on Win32 only require VS 2005

--- trunk/glint/make.bat
+++ trunk/glint/make.bat
@@ -1,29 +1,25 @@
 @echo OFF
-:: Script that first initializes environment before running GNU make on Win32.
-:: Necessary because make and other build tools are generally not in a Win32
-:: user's path.
 
-:: Isolate the user's environment from the build environment.
-:: This keeps builds consistent between machines, and avoids
-:: possible conflicts with the user's existing environment.
 setlocal
 
-set PATH=%SYSTEMROOT%\system32
+if "_%VS80COMNTOOLS%_" == "__" (
+  echo Need Visual Studio 2005 to build this package.
+)
 
-set THIRD_PARTY_ROOT=%~dp0..\..\..\..\third_party
+if "_%VCINSTALLDIR%_" == "__" (
+  call "%VS80COMNTOOLS%vsvars32.bat"
+)
 
-set SETUP_ENV_PLATFORMSDK_VC80=
-call %THIRD_PARTY_ROOT%\platformsdk_vc80\setup_env.bat
+if "_%VCINSTALLDIR%_" == "__" (
+  echo Please run vcvarsall.bat from VS 2005 installation to set uo compile environment.
+  goto END
+)
 
-set SETUP_ENV_VC_80=
-call %THIRD_PARTY_ROOT%\vc_80\setup_env.bat
+set THIRD_PARTY_ROOT=%~dp0third_party
 
-set SETUP_ENV_GNU_TOOLS=
-call %THIRD_PARTY_ROOT%\gnu\setup_env.bat
+call %THIRD_PARTY_ROOT%\gnu_tools_win32\setup_env.bat
 
-set SETUP_ENV_PYTHON_24=
-call %THIRD_PARTY_ROOT%\python_24\setup_env.bat
+%THIRD_PARTY_ROOT%\gnu_tools_win32\files\make.exe %*
 
-%THIRD_PARTY_ROOT%\gnu\files\make.exe %*
-
+:END
 endlocal
